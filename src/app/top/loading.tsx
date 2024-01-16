9cf63bd10ea10c850857c1ecbffb8eace80809a7
Adam <adam.mehdaouijorge@gmail.com>
2024-01-16 22:41:12 +0100

    working with loading component

---
+++ src/app/top/loading.tsx
@@ -0,0 +1,5 @@
+export default function Loading() {
+  return (
+      <h1 className="mt-5 ml-5">Processing...</h1>
+  )
+}
---
+++ src/components/header.tsx
@@ -0,0 +1,31 @@
+import Link from "next/link"
+import Brand from "./brand"
+
+export default function Header() {
+  return (
+    <header className="flex bg-heaven-blue pb-2">
+      <div className="flex ml-20">
+        <Link href="/">
+          <Brand />
+        </Link>
+        <div className="flex mt-3 text-lg text-heaven-white">
+          <div className="ml-5">
+            <Link href="/top">
+              Top
+            </Link>
+          </div>
+          <div className="ml-5">
+            <Link href="/schedules">
+              Schedules
+            </Link>
+          </div>
+          <div className="ml-5">
+            <Link href="/about">
+              About
+            </Link>
+          </div>
+        </div> 
+      </div>
+    </header>
+  )
+}
