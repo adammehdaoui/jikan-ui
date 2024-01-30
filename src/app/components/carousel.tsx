cbea54275004c52c2771d2c4416080c643624f2b
Adam <adam.mehdaouijorge@gmail.com>
2024-01-30 12:30:23 +0100

    auto switch

--- src/app/components/carousel.tsx
+++ src/app/components/carousel.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-import { useMemo, useCallback, useState } from "react"
+import { useMemo, useCallback, useState, useEffect } from "react"
 import { data } from "@/data/carouselData"
 import CarouselCard from "@/components/carouselCard"
 import Image from "next/image"
@@ -10,46 +10,59 @@ export default function Carousel() {
   const [displayedCard, setDisplayedCard] = useState(1)
 
   const idList = useMemo(() => data.map((item) => item.id), [])
-  const maxId = useMemo(() => Math.max(...idList), [idList])
   const minId = useMemo(() => Math.min(...idList), [idList])
+  const maxId = useMemo(() => Math.max(...idList), [idList])
 
-  const handleClick = useCallback(
+  const handleSlideChange = useCallback(
     (type: "previous" | "next") => {
-      if (type === "previous" && minId < displayedCard) {
-        setDisplayedCard(displayedCard - 1)
-      } else if (type === "next" && maxId > displayedCard) {
-        setDisplayedCard(displayedCard + 1)
+      if (type === "previous") {
+        minId < displayedCard
+          ? setDisplayedCard(displayedCard - 1)
+          : setDisplayedCard(maxId)
+      } else if (type === "next") {
+        maxId > displayedCard
+          ? setDisplayedCard(displayedCard + 1)
+          : setDisplayedCard(minId)
       }
     },
     [displayedCard, minId, maxId],
   )
 
+  useEffect(() => {
+    const intervalID = setInterval(() => handleSlideChange("next"), 5000)
+
+    return () => clearInterval(intervalID)
+  }, [handleSlideChange])
+
   return (
     <div className="flex justify-center mt-32 w-full">
-      <div className="flex w-96">
-        <button aria-label="switch to previous" className="w-14">
-          <Image
-            src="/assets/svg/switch.svg"
-            width={500}
-            height={500}
-            alt="switch"
-            className="rotate-180"
-            onClick={() => handleClick("previous")}
-          />
-        </button>
-        <div className="relative">
-          <div className="flex">
-            {data.map((item) => (
-              <div key={item.id}>
-                <CarouselCard
-                  animeInfo={{ title: item.title, link: item.link }}
-                  path={item.image}
-                  hidden={item.id === displayedCard ? false : true}
-                />
-              </div>
-            ))}
-          </div>
-          <div className="absolute flex justify-evenly w-1/3 bottom-10 left-5 z-10">
+      <div className="flex w-96 relative">
+        <div>
+          <button
+            aria-label="switch to previous"
+            className="absolute w-8 left-0 top-1/2 z-20"
+          >
+            <Image
+              src="/assets/svg/switch.svg"
+              width={500}
+              height={500}
+              alt="switch"
+              className="rotate-180"
+              onClick={() => handleSlideChange("previous")}
+            />
+          </button>
+        </div>
+        <div className="flex">
+          {data.map((item) => (
+            <div key={item.id}>
+              <CarouselCard
+                animeInfo={{ title: item.title, link: item.link }}
+                path={item.image}
+                hidden={item.id === displayedCard ? false : true}
+              />
+            </div>
+          ))}
+          <div className="absolute flex justify-evenly w-1/4 bottom-10 left-5 z-10">
             {data.map((item, index) => (
               <div
                 key={`${item.title}_${index}`}
@@ -63,15 +76,20 @@ export default function Carousel() {
             ))}
           </div>
         </div>
-        <button aria-label="switch to next" className="w-14">
-          <Image
-            src="/assets/svg/switch.svg"
-            width={500}
-            height={500}
-            alt="switch"
-            onClick={() => handleClick("next")}
-          />
-        </button>
+        <div>
+          <button
+            aria-label="switch to next"
+            className="absolute w-8 right-0 top-1/2 z-20"
+          >
+            <Image
+              src="/assets/svg/switch.svg"
+              width={500}
+              height={500}
+              alt="switch"
+              onClick={() => handleSlideChange("next")}
+            />
+          </button>
+        </div>
       </div>
     </div>
   )
