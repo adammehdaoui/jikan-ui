1c609c2c908fe9519d423fc06631db8234f2dfbf
Adam <adam.mehdaouijorge@gmail.com>
2024-02-01 23:17:36 +0100

    opti avec useReducer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,11 @@
 import Carousel from "@/components/carousel"
-import Today from "@/components/today"
+import TodaySchedules from "@/components/todaySchedules"
 
 export default function Page() {
   return (
     <div className="flex justify-center mt-32">
       <Carousel />
-      <Today />
+      <TodaySchedules />
     </div>
   )
 }
