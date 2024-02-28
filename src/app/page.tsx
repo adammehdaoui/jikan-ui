5eaa2c5f18b4873c8d2abb5bacd86d51b3d0bcec
Adam <adam.mehdaouijorge@gmail.com>
2024-02-28 15:30:03 +0100

    resolving conflicts

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,16 @@
 import Carousel from "@/components/carousel"
+import MainContainer from "@/components/interface/mainContainer"
+import Footer from "@/components/layout/footer"
 import TodaySchedules from "@/components/todaySchedules"
 
 export default function Page() {
   return (
-    <div className="flex flex-col items-center w-full lg:flex-row lg:justify-center mt-32 md:mt-48">
-      <Carousel />
-      <TodaySchedules />
+    <div className="flex flex-col relative mt-32 md:mt-48">
+      <MainContainer>
+        <Carousel />
+        <TodaySchedules />
+      </MainContainer>
+      <Footer />
     </div>
   )
 }
